bc6cb068f05c6cdecebae530427f0f9ea87af7ec
jizz <jarrod.beardwood@me.com>
2012-12-05 12:10:01 +1100

    cleaned up trailing whitespace

--- ftInTitle.py
+++ ftInTitle.py
@@ -39,14 +39,14 @@ class ftInTitle(BeetsPlugin):
         def func(lib, config, opts, args):
 	  for track in lib.items():
 	    artistfield  = track.__getattr__("artist")
-	    regxRes = re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|[wW]ith', artistfield)
+	    regxRes = re.split('\s[fF]t\.|\s[fF]eaturing|\s[fF]eat\.|\s[wW]ith', artistfield)
 	    if len(regxRes)>1:
 	      titleField = track.__getattr__("title")
 	      featInTitle = re.search('[fF]t\.|[fF]eaturing|[fF]eat\.', titleField)
 	      if featInTitle==None:
 		print track.__getattr__("path")
 		track.__setattr__("artist", regxRes[0])
-		track.__setattr__("title", titleField + " feat." + regxRes[1])
+		track.__setattr__("title", titleField + " (feat." + regxRes[1] + ")")
 		track.write()
 	      else:
 		track.__setattr__("artist", regxRes[0])	      
